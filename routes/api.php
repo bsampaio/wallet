97538074d649422f6d451d758e1c9b7c5be7e987
Breno Grillo <breno.grillo@lifepet.com.br>
2021-06-28 23:09:46 -0300

    Adicionando arquivo do Insomnia

--- routes/api.php
+++ routes/api.php
@@ -97,7 +97,7 @@ Route::middleware('heimdall')->group(function() {
 
 Route::get('/notifications/juno', function(Request $request) {
     \Illuminate\Support\Facades\Log::info('notifications.juno.get', ['request' => $request->all()]);
-});
+})->name('integrations.juno.notifications.get');;
 Route::post('/notifications/juno', function(Request $request) {
     \Illuminate\Support\Facades\Log::info('notifications.juno.post', ['request' => $request->all()]);
-});
+})->name('integrations.juno.notifications.post');
