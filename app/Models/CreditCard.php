c433b816268f3fd1463a116bef112b7170fe12ab
Breno Grillo <breno.grillo@lifepet.com.br>
2021-08-13 14:22:36 -0300

    bugfix: Permitindo softDelete em cartões de crédito

--- app/Models/CreditCard.php
+++ app/Models/CreditCard.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class CreditCard
@@ -30,6 +31,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class CreditCard extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'cards';
 
